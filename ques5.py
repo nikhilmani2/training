7b8344ff93b648f56df1fb35dadccefa4efa7067
nikhilmani2 <nimani.ext@deloitte.com>
2023-09-25 18:07:35 +0530

    Update ques5.py

--- ques5.py
+++ ques5.py
@@ -19,4 +19,37 @@ class Bank:
             self.checkbalance(ac_number)
         else:
             print("Account is not there")    
-                           
+    def withdrawl(self,ac_number,amount):
+        if ac_number in self.accounts:
+            if amount>0 :
+                if self.accounts[ac_number]>=amount:
+                    self.checkbalance(ac_number)
+                    self.accounts[ac_number]-=amount
+                    print("Withdrawl Successfull")
+                    self.checkbalance(ac_number)
+                else:
+                    print("Withrawal amount is more than balance")
+            else:
+                print("Amount must be positive")
+        else:
+            print("Account is not there")                           
+b1=Bank()
+
+b1.create("1001",1500)
+b1.create("1002")  
+b1.create("1003",100000)
+print(b1.checkbalance("1001"))
+print(b1.checkbalance("1002"))
+print(b1.checkbalance("1003"))
+b1.deposite("1002",5000)
+print(b1.checkbalance("1001"))
+print(b1.checkbalance("1002"))
+print(b1.checkbalance("1003"))
+b1.withdrawl("1003",7000)
+print(b1.checkbalance("1001"))
+print(b1.checkbalance("1002"))
+print(b1.checkbalance("1003"))
+b1.withdrawl("1001",100000)
+print(b1.checkbalance("1001"))
+print(b1.checkbalance("1002"))
+print(b1.checkbalance("1003"))               
