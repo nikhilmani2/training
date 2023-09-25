68da98b95297318855ea92f98ac7bf635af27d47
nikhilmani2 <145959576+nikhilmani2@users.noreply.github.com>
2023-09-25 12:10:39 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,13 @@
+import math
+class circle:
+    def __init__(self,r):
+        self.r=r
+    def area(self):
+        return (math.pi)*((self.r)**2)
+        
+    def perimeter(self):
+        return 2*math.pi*self.r
+
+c1=circle(7)
+print(c1.area())
+print(c1.perimeter())
