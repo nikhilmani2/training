a89adac797c689ba620383c43c55a83e8edbefc1
nikhilmani2 <nimani.ext@deloitte.com>
2023-09-28 10:56:25 +0530

    questions

---
+++ 28sept/problem2.py
@@ -0,0 +1,16 @@
+import math
+class Cylinder:
+    
+    def __init__(self,height=1,radius=1):
+        self.height=height
+        self.radius=radius
+    def volume(self):
+        return (math.pi)*(self.radius **2)*(self.height)
+    
+    def surface_area(self):
+        return ((2*math.pi*(self.radius**2))+(2*math.pi*self.radius*self.height))
+        
+    
+c  = Cylinder(2,3)
+print(c.volume())   
+print(c.surface_area()) 
